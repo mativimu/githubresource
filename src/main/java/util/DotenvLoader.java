441948733aa96e337f56357166e13b3b46155e53
mativimu <mativimu.u@gmail.com>
2023-03-17 01:15:56 -0300

    DotenvLoader upgraded

--- src/main/java/util/DotenvLoader.java
+++ src/main/java/util/DotenvLoader.java
@@ -1,13 +1,9 @@
 package main.java.util;
 
-import java.nio.file.Paths;
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class DotenvLoader {
     
-    private Dotenv dotenv;
-
     public static Dotenv newInstance() {
         return Dotenv.configure().directory("src/main/resources/").load();
     }
